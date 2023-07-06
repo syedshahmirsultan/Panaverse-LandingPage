5efa686316a92122530bbd1b26b286d086cdae8e
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-07-07 02:59:36 +0500

    Update Header.tsx

--- app/Components/Layout/Header.tsx
+++ app/Components/Layout/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <Wrapper>
     <div className="flex justify-between mx-auto max-w-screen-xl  py-4 sticky top-0 items-center">
    <div><Image src="/images/panaverselogo.png" alt="Panaverse Dao Logo" height={120} width={120}/></div> 
-        <ul className="flex space-x-8 font-medium text-2xl">
+        <ul className="flex space-x-8 font-medium text-xl">
         <li><Link href="/">Home</Link></li>
         <li><Link href="/courses">Courses</Link></li>
         </ul></div></Wrapper></header>
