fe2ba71de0c5105acd0628a0df4f720ff4bf1e08
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-07-07 02:41:27 +0500

    Update Specialized.tsx

--- app/Components/sections/Specialized.tsx
+++ app/Components/sections/Specialized.tsx
@@ -1,50 +1,120 @@
 'use client'
-import React, { useState } from 'react';
-import QuarterBox from '../Shared/QuarterBox';
+import React, { useState } from 'react'
+import QuarterBox from '../Shared/QuarterBox'
 import Wrapper from '../Shared/Wrapper';
 import Image from 'next/image';
+const data=[
+  {
+    slug:"wmd",
+      header:"Web 3.0 (Blockchain) and Metaverse Specialization",
+      description:"This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
+      src:"/images/metaverse.png",
+      alt:"Metaverse",
+      quarters:[{
+     header:"Quarter IV",
+     description:"W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform",
+     number:4
+      },
+  {
+      header:"Quarter V",
+     description:"W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform",
+     number:5
 
-const data = [
-  {
-    slug: "wmd",
-    header: "Web 3.0 (Blockchain) and Metaverse Specialization",
-    description: "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences.",
-    src: "/images/metaverse.png",
-    alt: "Metaverse",
-    quarters: [
-      {
-        header: "Quarter IV",
-        description: "W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform",
-        number: 4
-      },
-      {
-        header: "Quarter V",
-        description: "W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform",
-        number: 5
-      }
-    ]
+  }]
+
   },
-  {
-    slug: "AI",
-    header: "Artificial Intelligence (AI) and Deep Learning Specialization",
-    description: "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
-    src: "/images/ai.png",
-    alt: "AI",
-    quarters: [
-      {
-        header: "Quarter IV",
-        description: "AI-351: Developing Planet Scale Intelligent APIs and Python Programming",
-        number: 4
+  { slug:"AI",
+      header:"Artificial Intelligence (AI) and Deep Learning Specialization",
+      description:"The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+      src:"/images/ai.png",
+      alt:"AI",
+      quarters:[{
+     header:"Quarter IV",
+     description:"AI-351:Developing Planet Scale Intelligent APIs and Python Programming",
+     number:4
       },
-      {
-        header: "Quarter V",
-        description: "AI-361: Deep Learning and MLOps",
-        number: 5
-      }
-    ]
+  {
+      header:"Quarter V",
+     description:"AI-361: Deep Learning and MLOps",
+     number:5
+
+  }]
+
   },
-  // Rest of the data objects...
-];
+  { slug:"Cloud",
+      header:"Cloud-Native Computing Specialization",
+      description:"The Cloud-Native Computing Specialization focuses on Containers, Kubernetes, and CDK for Kubernetes.",
+      src:"/images/cloud.png",
+      alt:"Cloud",
+      quarters:[{
+     header:"Quarter IV",
+     description:"CN-351: Certified Kubernetes Application Developer (CKAD)",
+     number:4
+      },
+  {
+      header:"Quarter V",
+     description:"CN-361: Developing Multi-Cloud Apps using CDK for Terraform",
+     number:5
+
+  }]
+
+  },
+  { slug:"Ambient",
+      header:"Ambient Computing and IoT Specialization",
+      description:"The Ambient Computing and IoT Specialization focuses on building Smart Homes, Offices, Factories, and Cities using Voice computing, Matter Protocol, and Embedded Devices.",
+      src:"/images/ambient.png",
+      alt:"Ambient Computting",
+      quarters:[{
+     header:"Quarter IV",
+     description:"AC-351: Ambient Computing with Voice Assistants and Matter Devices",
+     number:4
+      },
+  {
+      header:"Quarter V",
+     description:"AC-361: Embedded Programming using C and Rust",
+     number:5
+
+  }]
+
+  },
+  { slug:"Genomics",
+      header:"Genomics and Bioinformatics Specialization",
+      description:"Genomics is the study of the total genetic makeup of individual organisms, and how this genetic information is structured, functions, and has evolved; bioinformatics encompasses a diverse range of analytical methods and tools applied to genomic data. This Specialization focuses on performing complex bioinformatics analysis using the most essential Python libraries and applications.",
+      src:"/images/genomics.png",
+      alt:"Genomics",
+      quarters:[{
+     header:"Quarter IV",
+     description:"Bio-351: Python for Biologists",
+     number:4
+      },
+  {
+      header:"Quarter V",
+     description:"Bio-361: Bioinformatics with Python",
+     number:5
+
+  }]
+
+  },
+  { slug:"Network",
+      header:"Network Programmability and Automation Specialization",
+      description:"More than ever, network engineers are finding it challenging to complete their duties entirely manually. Network automation is now crucial due to new protocols, technologies, delivery models, and the requirement for enterprises to become more adaptable and agile. This course teaches network engineers how to automate systems with code using a variety of technologies and tools, including Linux, Python, APIs, and Git.",
+      src:"/images/network.png",
+      alt:"Network Programmability",
+      quarters:[{
+     header:"Quarter IV",
+     description:"NPA-351: CCNA 200-301 Certification",
+     number:4
+      },
+  {
+      header:"Quarter V",
+     description:"NPA-361: Network Programmability and Automation",
+     number:5
+
+  }]
+
+  }
+  
+]
 
 function Specialized() {
   const [selectedItem, setSelectedItem] = useState("wmd");
